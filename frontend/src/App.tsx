0d64de3bbb5d715aefd552ff27a40136e1e0551e
Kacem Mathlouthi <kacem.mathlouthi@insat.ucar.tn>
2025-06-02 23:14:18 +0100

    Feat: Frontend Structure Init (Typescript, React, Vite, Tailwindcss)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        VCell Frontend Test
+      </h1>
+    </div>
+  );
+}
+
+export default App
