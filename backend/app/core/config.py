48c382a56ebf74abdbcca5f576c4f420efce8ae1
Kacem Mathlouthi <kacem.mathlouthi@insat.ucar.tn>
2025-06-03 14:58:08 +0100

    Feat: Setting loads .env keys automatically using pydantic_settings

---
+++ backend/app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    # Frontend Config
+    FRONTEND_URL: str
+
+    class Config:
+        env_file = ".env"
+        cache_on_load = False
+
+settings = Settings()
