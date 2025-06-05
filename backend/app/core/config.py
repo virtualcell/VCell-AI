bf4b0cc1e160a46d5517f2399f99e99bc4646149
Kacem Mathlouthi <kacem.mathlouthi@insat.ucar.tn>
2025-06-05 01:37:22 +0100

    Feat: Azure OpenAI Env Config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,6 +5,12 @@ class Settings(BaseSettings):
     # Frontend Config
     FRONTEND_URL: str
 
+    # Azure OpenAI Config
+    AZURE_API_VERSION: str
+    AZURE_ENDPOINT: str
+    AZURE_API_KEY: str
+    AZURE_DEPLOYMENT_NAME: str
+
     class Config:
         env_file = ".env"
         cache_on_load = False
