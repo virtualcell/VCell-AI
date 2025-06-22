c658ab72406542136b9480b8d1ebc19ef19357b6
Kacem Mathlouthi <kacem.mathlouthi@insat.ucar.tn>
2025-06-22 19:08:47 +0100

    Fix: Update LLM prompts for more conciseness.

--- backend/app/services/llms_service.py
+++ backend/app/services/llms_service.py
@@ -127,7 +127,7 @@ async def analyse_vcml(biomodel_id: str):
         # Fetch VCML details
         vcml = await get_vcml_file(biomodel_id)
         # Analyze VCML with LLM
-        vcml_system_prompt = "You are a VCell BioModel Assistant, designed to help users understand and interact with biological models in VCell. Your task is to provide human-readable, accurate responses based on the given VCML."
+        vcml_system_prompt = "You are a VCell BioModel Assistant, designed to help users understand and interact with biological models in VCell. Your task is to provide human-readable, concise responses based on the given VCML."
         vcml_prompt = f"Analyze the following VCML content for Biomodel {biomodel_id}: {str(vcml)}"
         vcml_analysis = await get_llm_response(vcml_system_prompt, vcml_prompt)
         return vcml_analysis
@@ -206,7 +206,7 @@ async def analyse_diagram(biomodel_id: str):
         diagram_analysis_prompt = (
             "You are a VCell BioModel Assistant, designed to help users understand and interact with biological models in VCell. "
             + biomodel_info
-            + "Your task is to analyze the diagram of the biomodel and provide a description of its components, interactions, and any other relevant information. "
+            + "Your task is to analyze the diagram of the biomodel and provide a concise description of its components, interactions, and any other relevant information. "
         )
         diagram_analysis_prompt = [
             {"type": "text", "text": diagram_analysis_prompt},
