269f39174c557de0d3bcf823ce3e7b85c6af3915
Kacem Mathlouthi <kacem.mathlouthi@insat.ucar.tn>
2025-07-26 00:00:42 +0100

    Feat: add knowledge base router with endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,6 +10,7 @@ logger = get_logger(__file__)
 from app.routes.vcelldb_router import router as vcelldb_router
 from app.routes.llms_router import router as llms_router
 from app.routes.qdrant_router import router as qdrant_router
+from app.routes.knowledge_base_router import router as knowledge_base_router
 
 ascii_art = """
 ╔════════════════════════════════════════════════════════════════════════════════════╗
@@ -34,8 +35,9 @@ app.add_middleware(
 )
 
 # Including the routers
-app.include_router(vcelldb_router, tags=["VCellDB API Wrapper"])
+app.include_router(knowledge_base_router, tags=["Knowledge Base"], prefix="/kb")
 app.include_router(llms_router, tags=["LLM with Tool Calling"])
+app.include_router(vcelldb_router, tags=["VCellDB API Wrapper"])
 app.include_router(qdrant_router, tags=["Qdrant Vector DB"], prefix="/qdrant")
 
 if __name__ == "__main__":
