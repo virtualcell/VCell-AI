c00d0acc6db5f974dd9f3c7285ad823dedbdaaae
Ezequiel Valencia <ezq.valencia@gmail.com>
2025-09-03 14:12:38 -0400

    Remove CORS Config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -8,21 +8,6 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
-  },
-  async headers() {
-    return [
-      {
-        // Matches all API routes
-        source: "/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          // Replace with your allowed origin(s) or "*" for all
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
   }
 };
 
