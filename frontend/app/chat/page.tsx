6b2eaa9a057ec1635a7a0f33f900186e67615104
kartik <kartikdeshpande2810@gmail.com>
2026-03-08 18:09:26 +0530

    fixed typo

--- frontend/app/chat/page.tsx
+++ frontend/app/chat/page.tsx
@@ -77,9 +77,9 @@ export default function ChatPage() {
       value: "How to create an account on VCell Software?",
     },
     {
-      label: "How to model FrapBindings in VCell Software?",
+      label: "How to model FRAP Bindings in VCell Software?",
       icon: <FileText className="h-3 w-3 mr-2" />,
-      value: "How to model FragBindings in VCell Software?",
+      value: "How to model FRAP Bindings in VCell Software?",
     },
     {
       label: "How to model Moving Boundaries in VCell Software?",
